e3ee2563f27295d031eecc91171888669f9d6513
betussozgen <fatmabetulozgen@hotmail.com>
2023-05-08 15:56:28 +0300

    created hook page

---
+++ src/hooks/useFetch/useFetch.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+function useFetch(url) {
+
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const { data: responseData } = await axios.get(url);
+            setData(responseData)
+            setLoading(false);
+        } catch (error) {
+            setError(err.message);
+            setLoading(false);
+        }
+
+
+    }
+
+    return { error, loading, data };
+
+
+}
+
+
+
+export default useFetch;
